7150ca28720a96d66713c145b5178cb1ca3f8060
sigenogoro <25719873+sigenogoro@users.noreply.github.com>
2023-08-08 23:58:23 +0900

    const は再代入はできないが、オブジェクトのプロパティに対して可変である

--- build/sample.js
+++ build/sample.js
@@ -1,13 +1,4 @@
 "use strict";
-class Point {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    add(point) {
-        return new Point(this.x + point.x, this.y + point.y);
-    }
-}
-var p1 = new Point(0, 10);
-var p2 = new Point(10, 20);
-var p3 = p1.add(p2); // {x:10,y:30}
+const obj = { a: "aaa" };
+obj.a = "bbb";
+console.log(obj);
